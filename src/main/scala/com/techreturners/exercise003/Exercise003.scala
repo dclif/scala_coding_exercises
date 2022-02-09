372efb47347e6c7f8c37d4bbfd206f26a07d13d0
Daniel <65896341+dclif@users.noreply.github.com>
2022-02-09 14:47:12 +0000

    Exercise 3 tests passing

--- src/main/scala/com/techreturners/exercise003/Exercise003.scala
+++ src/main/scala/com/techreturners/exercise003/Exercise003.scala
@@ -3,11 +3,30 @@ package com.techreturners.exercise003
 class Exercise003 {
 
   def getIceCreamCode(iceCreamFlavour: String): Int = {
+    val flavours = Array("Pistachio",
+      "Raspberry Ripple",
+      "Vanilla",
+      "Mint Chocolate Chip",
+      "Chocolate",
+      "Mango Sorbet")
+    return flavours.indexOf(iceCreamFlavour)
+
     throw new UnsupportedOperationException("You can delete this line and add your code here.")
+
+
   }
 
   def iceCreamFlavours: Array[String] = {
+    val flavours = Array("Pistachio",
+      "Raspberry Ripple",
+      "Vanilla",
+      "Mint Chocolate Chip",
+      "Chocolate",
+      "Mango Sorbet"
+    )
+    return flavours
     throw new UnsupportedOperationException("You can delete this line and add your code here.")
+
   }
 
 }

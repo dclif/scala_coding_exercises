2ecc865e75012200902fd9ec94011cc072e8818a
Daniel <65896341+dclif@users.noreply.github.com>
2022-02-09 17:53:14 +0000

    be returned with a date and time over a million years ago

--- src/test/scala/com/techreturners/exercise004/Exercise004Test.scala
+++ src/test/scala/com/techreturners/exercise004/Exercise004Test.scala
@@ -8,7 +8,7 @@ class Exercise004Test extends AnyFlatSpec{
 
   "A correct date time" should "be returned when constructed with a date" in {
 
-    // Try un-commenting the code in this test
+    // Try  un-commenting the code in this test
     // it will fail to compile because the Exercise004 constructor takes a LocalDateTime
     // Have a look in Exercise004 and see if you can un-comment and get this test passing
     // val exercise004 = new Exercise004(LocalDate.of(2021, Month.JULY, 19))
